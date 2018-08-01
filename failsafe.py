28cd060e8e206c3e2459d8088e0a608947ff9ac9
ben <ben@jianmin.ninja>
2018-08-01 12:51:19 -0400

    modifications based on quad testing

--- failsafe.py
+++ failsafe.py
@@ -47,31 +47,31 @@ class failsafe_and_stuff:
         if ping.do_one(self.base_ip,self.ping_timeout) != None:
           time.sleep(self.ping_delay)
         else: 
-          self.trigger_failsafe()
+          self.trigger_failsafe(source="ping")
 
-  def trigger_failsafe(self):
-    self.debug("failsafe activated")
-    #os.system("reboot")
+  def trigger_failsafe(self,source=""):
+    self.debug("failsafe activated %s" % source)
+    os.system("reboot")
     
   def trigger_deauth(self):
     self.debug("deauth activated")
-    #os.system("ps -ef | egrep deauth.s[h] | egrep -v grep || bash deauth.sh")
+    os.system("ps -ef | egrep deauth.s[h] | egrep -v grep || bash deauth.sh")
 
   def udp_listener(self):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(("0.0.0.0",self.cmd_udp_port))
     while not self.safe_exit:
       data, addr = sock.recvfrom(1)
-      print "received message:", data
       if addr[0] == self.base_ip:
         self.last_udp_heartbeat = time.time()
       if data == "A":
-        self.udp_connnected = True
+	if self.udp_connected == False:
+	  print "starting udp failsafe watcher"
+        self.udp_connected = True
         self.connnected = True
         pass #alive
       elif data == "K":
-        if self.udp_connected:
-          self.add_thread_and_start(threading.Thread(target=self.trigger_failsafe)) # kill
+        self.add_thread_and_start(threading.Thread(target=self.trigger_failsafe)) # kill
       elif data == "D":
         self.add_thread_and_start(threading.Thread(target=self.trigger_deauth)) # deauth
       elif data == "Z":
@@ -83,8 +83,8 @@ class failsafe_and_stuff:
     self.last_udp_heartbeat = time.time()
     while not self.safe_exit:
       time.sleep(0.5)
-      if time.time() - self.last_udp_heartbeat > self.cmd_delay:
-        self.trigger_failsafe()
+      if time.time() - self.last_udp_heartbeat > self.cmd_delay and self.udp_connected:
+        self.trigger_failsafe(source="udp")
     
       
 if __name__ == "__main__":
--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cd /home/pi/dev
+mkdir -p logs
+( /usr/bin/stdbuf -oL /home/pi/dev/failsafe.py > "logs/$(date)_failsafe.log")&
 
-( /home/pi/dev/failsafe.py > failsafe.log)&
 
-
-( /home/pi/dev/camera.sh > camera.log ) &
+( /usr/bin/stdbuf -oL /home/pi/dev/camera.sh > "logs/$(date)_camera.log" ) &
